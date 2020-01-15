6614264d144b53a97f4e3f2cff93eb7af196ef56
andres <aperez@kdu.cl>
2020-01-14 22:50:44 -0300

    prueba

--- src/components/PokemonEntry.js
+++
@@ -1,8 +0,0 @@
-import react, { Fragment } from 'react';
-
-const PokemonEntry=(props)=>{
-return <Fragment>
-    <img src={props.image}></img> <span>{props.name}</span>
-</Fragment>
-}
-export default PokemonEntry;
